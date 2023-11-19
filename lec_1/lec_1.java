886c6578cb397afd53c7c259f032ca164c7904ff
Elizaveta <Elizavetabelyankina@mail.ru>
2023-11-19 19:51:13 +0300

    g

---
+++ lec_1/lec_1.java
@@ -0,0 +1,10 @@
+package lec_1;
+
+/**
+ * lec_1
+ */
+public class lec_1 {
+    public static void main(String[] args) {
+        
+    }
+}
--- lec_1/sem_1.java
+++ lec_1/sem_1.java
@@ -1,5 +1,7 @@
 package lec_1;
 
 public class sem_1 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
